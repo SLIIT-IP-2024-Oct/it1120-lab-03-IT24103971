6557ab73f5d26f3bf29fd2667167d024418fe2bc
IT24103971 <it24103971@my.sliit.lk>
2024-11-06 13:07:38 +0530

    Add files via upload

---
+++ Q1A/IT24103971Lab3Q1A.java
@@ -0,0 +1,11 @@
+public class IT24103971Lab3Q1A {
+    public static void main(String[] args) {
+        // Enter price for per kilogram of rice
+        double pricePerKg = 225;
+        // Enter the price for number of kilograms you want to buy
+        double numOfKilograms = 5;
+        // Calculate the total amount
+        double totalAmount = pricePerKg*numOfKilograms;
+        System.out.println("The total amount is " + totalAmount);
+    }
+} 
